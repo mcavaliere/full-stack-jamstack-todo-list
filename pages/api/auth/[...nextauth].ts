a6a6ecef341ed8c10e8ce1910c04bc785ac55b0d
Mike Cavaliere <mike@cavaliere.org>
2022-02-09 15:46:29 -0500

    chore: adds next-auth config

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+
+  secret: process.env.SECRET,
+
+  callbacks: {
+    async session({ session, user }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+        },
+      };
+    },
+  },
+});
