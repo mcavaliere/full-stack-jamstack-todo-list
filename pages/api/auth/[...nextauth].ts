40ff56b8753ca456d0b56bcd308cb3c43dc9c5ca
Mike Cavaliere <mike@cavaliere.org>
2022-02-11 16:03:01 -0500

    feat: account persistence, user details in session, missing Account field

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,12 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+import prisma from "../../../lib/server/prismaClientInstance";
 
 export default NextAuth({
-  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
@@ -12,13 +16,17 @@ export default NextAuth({
 
   secret: process.env.SECRET,
 
+  session: {
+    strategy: "database",
+  },
+
   callbacks: {
     async session({ session, user }) {
       return {
         ...session,
         user: {
           ...session.user,
-          id: user?.id,
+          ...user,
         },
       };
     },
